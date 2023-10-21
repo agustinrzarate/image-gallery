f3d1217a5682f4a150e1096d976ba0d4f30c9280
Agustin R Zarate <agustinrzarate@gmail.com>
2023-10-21 14:31:01 -0300

    setting theme and alias to import components

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,14 @@
-import './App.css';
+import { Button } from '@ui/components/Button/Button';
+import { ThemeProvider } from '@/ui/components/ThemeProvider/ThemeProvider';
 
 function App() {
-  return <div className=" text-4xl font-bold">hi</div>;
+  return (
+    <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
+      <div className=" ">
+        <Button>Hi</Button>
+      </div>
+    </ThemeProvider>
+  );
 }
 
 export default App;
---
+++ src/ui/components/MaxWidthWrapper/MaxWidthWrapper.tsx
@@ -0,0 +1,17 @@
+import { ReactNode } from 'react';
+import cn from '../../../app/lib/utils';
+
+interface MaxWidthWrapperProps {
+  className?: string;
+  children: ReactNode;
+}
+
+const MaxWidthWrapper = ({ className, children }: MaxWidthWrapperProps) => {
+  return <div className={cn('max-w-screen-2xl mx-auto w-full px-2.5 md:px-20', className)}>{children}</div>;
+};
+
+MaxWidthWrapper.defaultProps = {
+  className: '',
+};
+
+export default MaxWidthWrapper;
